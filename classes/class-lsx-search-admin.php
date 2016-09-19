e2658f005acc8fd68c920034492ecdb5aae105b9
Warwick Booth <warwick@lsdev.biz>
2016-09-19 14:38:39 +0200

    Merged one the rest of the search functions

--- classes/class-lsx-search-admin.php
+++ classes/class-lsx-search-admin.php
@@ -10,7 +10,6 @@ class LSX_Search_Admin extends LSX_Search{
 	 */
 	public function __construct() {
 		add_action('admin_init', array($this,'admin_init'));
-		add_action('lsx_framework_dashboard_tab_content',array($this,'settings'),11);
 	}
 
 	/**
@@ -27,31 +26,162 @@ class LSX_Search_Admin extends LSX_Search{
 	}	
 
 	/**
-	 * Outputs the dashboard tabs settings
+	 * A filter to set the layout to 2 column
+	 *
 	 */
-	public function settings() {
-		?>	
-			<tr class="form-field">
-				<th scope="row" colspan="2"><label><h3>LSX Search</h3></label></th>
-			</tr>	
-			<tr class="form-field">
-				<th scope="row">
-					<label for="text"><?php _e('Text Input','lsx-tour-operators'); ?></label>
-				</th>
-				<td>
-					<input type="text" {{#if text}} value="{{text}}" {{/if}} name="text" />
-				</td>
-			</tr>				
-			<tr class="form-field">
-				<th scope="row">
-					<label for="checkbox"><?php _e('Checkbox','lsx-tour-operators'); ?></label>
-				</th>
-				<td>
-					<input type="checkbox" {{#if checkbox}} checked="checked" {{/if}} name="checkbox" />
-					<small><?php _e('An example of a checkbox',$this->plugin_slug); ?></small>
-				</td>
-			</tr>				
-		<?php	
-	}	
+	public function search_settings($tab='general') { ?>
+		<tr class="form-field">
+			<th scope="row" colspan="2"><label><h3><?php _e('Search Settings',$this->plugin_slug); ?></h3></label></th>
+		</tr>		
+		<tr class="form-field">
+			<th scope="row">
+				<label for="enable_search"><?php _e('Enable Search',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<input type="checkbox" {{#if enable_search}} checked="checked" {{/if}} name="enable_search" />
+				<?php if('general' === $tab) { ?>
+					<small><?php _e('This adds the facet shortcodes to the search results template.',$this->plugin_slug); ?></small>
+				<?php }else{ ?>
+					<small><?php _e('This adds the facet shortcodes to the post type archive and taxonomy templates.',$this->plugin_slug); ?></small>
+				<?php } ?>
+			</td>
+		</tr>
+		<tr class="form-field-wrap">
+			<th scope="row">
+				<label for="search_layout"><?php _e('Layout',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<select value="{{search_layout}}" name="search_layout">
+					<option value="" {{#is search_layout value=""}}selected="selected"{{/is}}><?php _e('Follow the theme layout',$this->plugin_slug); ?></option>
+					<option value="1c" {{#is search_layout value="1c"}} selected="selected"{{/is}}>1 column</option>
+					<option value="2cr" {{#is search_layout value="2cr"}} selected="selected"{{/is}}>2 columns / Content on right</option>
+					<option value="2cl" {{#is search_layout value="2cl"}} selected="selected"{{/is}}>2 columns / Content on left</option>
+				?>
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="enable_price_sorting"><?php _e('Enable Price Sorting',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<input type="checkbox" {{#if enable_price_sorting}} checked="checked" {{/if}} name="enable_price_sorting" />
+				<small><?php _e('WARNING, any item that doesnt have a price will not show. ',$this->plugin_slug); ?></small>
+			</td>
+		</tr>		
+		<tr class="form-field">
+			<th scope="row">
+				<label for="display_result_count"><?php _e('Display Result Count',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<input type="checkbox" {{#if display_result_count}} checked="checked" {{/if}} name="display_result_count" />
+			</td>
+		</tr>			
+		<tr class="form-field-wrap">
+			<th scope="row">
+				<label for="facets"><?php _e('Facets',$this->plugin_slug); ?></label>
+			</th>
+			<td><ul>
+			<?php 	
+			if(is_array($this->facet_data) && !empty($this->facet_data)){
+
+				$active_facets = array();
+				if(isset($this->options[$tab]['facets']) && is_array($this->options[$tab]['facets'])){
+					$active_facets = $this->options[$tab]['facets'];
+				}
+
+				foreach( $this->facet_data as $facet){
+					?>
+					<li>
+						<input type="checkbox" <?php if(array_key_exists($facet['name'],$active_facets)){ echo 'checked="checked"'; } ?> name="facets[<?php echo $facet['name']; ?>]" /> <label for="facets"><?php echo $facet['label'].' ('.$facet['name'].')'; ?></label> 
+					</li>
+				<?php }
+			}else{
+				?>
+					<li><?php _e('You have no Facets setup.',$this->plugin_slug); ?></li>
+				<?php
+			}
+			?>
+			</ul></td>
+		</tr>
+
+		<?php
+	}
+
+	/**
+	 * A filter to set the layout to 2 column
+	 *
+	 */
+	public function archive_settings($tab='general') { 
+		if('general' !== $tab){
+		?>		
+		<tr class="form-field">
+			<th scope="row">
+				<label for="enable_facets"><?php _e('Enable Filtering',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<input type="checkbox" {{#if enable_facets}} checked="checked" {{/if}} name="enable_facets" />
+				<small><?php _e('This adds the facet shortcodes to the post type archive and taxonomy templates.',$this->plugin_slug); ?></small>
+			</td>
+		</tr>
+		<tr class="form-field-wrap">
+			<th scope="row">
+				<label for="archive_layout"><?php _e('Layout',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<select value="{{archive_layout}}" name="archive_layout">
+					<option value="" {{#is archive_layout value=""}}selected="selected"{{/is}}><?php _e('Follow the theme layout',$this->plugin_slug); ?></option>
+					<option value="1c" {{#is archive_layout value="1c"}} selected="selected"{{/is}}>1 column</option>
+					<option value="2cr" {{#is archive_layout value="2cr"}} selected="selected"{{/is}}>2 columns / Content on right</option>
+					<option value="2cl" {{#is archive_layout value="2cl"}} selected="selected"{{/is}}>2 columns / Content on left</option>
+				?>
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th scope="row">
+				<label for="enable_archive_price_sorting"><?php _e('Enable Price Sorting',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<input type="checkbox" {{#if enable_archive_price_sorting}} checked="checked" {{/if}} name="enable_archive_price_sorting" />
+				<small><?php _e('WARNING, any item that doesnt have a price will not show. ',$this->plugin_slug); ?></small>
+			</td>
+		</tr>		
+		<tr class="form-field">
+			<th scope="row">
+				<label for="display_archive_result_count"><?php _e('Display Result Count',$this->plugin_slug); ?></label>
+			</th>
+			<td>
+				<input type="checkbox" {{#if display_archive_result_count}} checked="checked" {{/if}} name="display_archive_result_count" />
+			</td>
+		</tr>			
+		<tr class="form-field-wrap">
+			<th scope="row">
+				<label for="facets"><?php _e('Facets',$this->plugin_slug); ?></label>
+			</th>
+			<td><ul>
+			<?php 	
+			if(is_array($this->facet_data) && !empty($this->facet_data)){
+
+				$active_facets = array();
+				if(isset($this->options[$tab]['archive_facets']) && is_array($this->options[$tab]['archive_facets'])){
+					$active_facets = $this->options[$tab]['archive_facets'];
+				}
+
+				foreach( $this->facet_data as $facet){
+					?>
+					<li>
+						<input type="checkbox" <?php if(array_key_exists($facet['name'],$active_facets)){ echo 'checked="checked"'; } ?> name="archive_facets[<?php echo $facet['name']; ?>]" /> <label for="facets"><?php echo $facet['label'].' ('.$facet['name'].')'; ?></label> 
+					</li>
+				<?php }
+			}else{
+				?>
+					<li><?php _e('You have no Facets setup.',$this->plugin_slug); ?></li>
+				<?php
+			}
+			?>
+			</ul></td>
+		</tr>
+		<?php 
+		}
+	}		
 }
 new LSX_Search_Admin();
--- classes/class-lsx-search-frontend.php
+++ classes/class-lsx-search-frontend.php
@@ -18,6 +18,27 @@ class LSX_Search_Frontend extends LSX_Search{
 	public function __construct() {
 		add_action('wp_head', array($this,'wp_head'));
 		add_action( 'wp_enqueue_scripts', array( $this, 'assets' ) );
+
+		//Redirects
+		add_action( 'template_redirect', array($this,'pretty_search_redirect') ) ;
+		add_filter( 'pre_get_posts',  array($this,'pretty_search_parse_query') ) ;
+
+		//Layout Filter
+		add_filter('lsx_layout', array($this,'lsx_layout'), 20,1);	
+
+		add_filter( 'facetwp_sort_options', array( $this,'facet_sort_options'), 10, 2 );
+		add_action('pre_get_posts', array($this,'price_sorting'),100);
+
+		
+		add_filter( 'facetwp_pager_html', array($this,'facetwp_pager_html'), 10, 2 );
+		add_filter( 'facetwp_result_count', array($this,'facetwp_result_count'), 10, 2 );
+		add_filter( 'facetwp_index_row', array($this,'facetwp_index_row'), 10, 2 );
+
+		add_shortcode( 'lsx_search_form', array($this,'search_form') );
+
+		add_filter( 'searchwp_short_circuit', array($this,'searchwp_short_circuit'), 10, 2 );
+
+		add_filter( 'get_search_query', array($this,'get_search_query') );				
 	}
 
 	/**
@@ -78,5 +99,449 @@ class LSX_Search_Frontend extends LSX_Search{
 		));
 		wp_localize_script( 'lsx_search', 'lsx_search_params', $params );		
 	}
+
+	/**
+	 * Rewrite the search URL
+	 */	
+	public function pretty_search_redirect() {
+		global $wp_rewrite,$wp_query;
+		if ( !isset( $wp_rewrite ) || !is_object( $wp_rewrite ) || !$wp_rewrite->using_permalinks() )
+			return;
+
+		$search_base = $wp_rewrite->search_base;
+		if ( is_search() && !is_admin() && strpos( $_SERVER['REQUEST_URI'], "/{$search_base}/" ) === false ) {
+			$search_query = get_query_var( 's' );
+			
+			$engine = '';
+			//If the search was triggered by a supplemental engine
+			if(isset($_GET['engine']) && 'default' !== $_GET['engine']){
+				$engine = $_GET['engine'];
+				set_query_var('engine',$engine);
+				$engine = array_search($engine,$this->post_type_slugs).'/';
+			}
+
+			wp_redirect( home_url( "/{$search_base}/". $engine . urlencode($search_query )) );
+			exit();
+		}
+	}
+
+	/**
+	 * Parse the Query and trigger a search
+	 */
+	public function pretty_search_parse_query( $query ) {
+		if ( is_search() && !is_admin() ) {
+			$search_query = $query->get('s');
+
+			$keyword_test = explode('/',$search_query);
+			if(count($keyword_test) > 1){
+
+				$engine = $this->post_type_slugs[$keyword_test[0]];
+
+				$query->set('s',$keyword_test[1]);
+				$query->set('engine',$engine);	
+
+				$additional_posts = new SWP_Query(
+					array(
+						's' => $keyword_test[1], // search query
+						'engine' => $engine,
+						'fields' => 'ids' 
+					)
+				);	
+
+				if ( ! empty( $additional_posts->posts ) ) {
+					$query->set('s','');
+					$query->set('engine_keyword',$keyword_test[1]);
+					$query->set('post__in',$additional_posts->posts);
+				}
+			}
+		}
+		return $query;
+	}
+
+	/**
+	 * A filter to set the layout to 2 column
+	 *
+	 */
+	public function lsx_layout($layout) {
+		if(false !== $this->search_slug){
+			if(is_search()){
+				$slug = 'search';
+				$id = 'search';
+			}else{
+				$slug='facets';
+				$id = 'archive';
+			}
+			if(false !== $this->options && isset($this->options[$this->search_slug]['enable_'.$slug]) && isset($this->options[$this->search_slug][$id.'_layout']) && '' !== $this->options[$this->search_slug][$id.'_layout']){
+				$layout = $this->options[$this->search_slug][$id.'_layout'];
+			}
+		}
+		return $layout;
+	}	
+
+	/**
+	 * Shortcircuit the main search if need be
+	 */
+	public function searchwp_short_circuit($maybe_short_circuit, $obj) {
+		$search_query = get_query_var('s');
+		$engine = get_query_var('engine');
+
+		if(false !== $engine && '' !== $engine && 'default' !== $engine){
+			$maybe_short_circuit = true;
+		}	
+		return $maybe_short_circuit;
+	}	
+
+	/**
+	 * Register the global post types.
+	 *
+	 *
+	 * @return    null
+	 */	
+	public function facet_sort_options( $options, $params ) {
+		global $wp_query;
+
+		unset($options['date_desc']);
+		unset($options['date_asc']);
+		unset($options['distance']);	
+
+		$search_slug = false;
+		if(is_search()){
+			$search_slug = 'general';
+			$option_slug = '';
+		}elseif(is_post_type_archive($this->post_types)||is_tax($this->taxonomies)){
+			$search_slug = get_post_type();
+			$option_slug = 'archive_';
+		}		
+
+		if(('default' === $params['template_name'] || 'wp' === $params['template_name'])
+			&& false !== $search_slug && false !== $this->options && isset($this->options[$search_slug]['enable_'.$option_slug.'price_sorting']) 
+			&& 'on' === $this->options[$search_slug]['enable_'.$option_slug.'price_sorting']) {
+
+			unset($options['default']);
+			
+			$new_options = array();
+			
+			$new_options['default'] = array(
+					'label' => __( 'Price (Highest)', 'lsx' ),
+					'query_args' => array(
+							'orderby' => 'meta_value_num',
+							'meta_key' => 'price',
+							'order' => 'DESC',
+					)
+			);
+		
+			$new_options['price_desc'] = array(
+					'label' => __( 'Price (Lowest)', 'lsx' ),
+					'query_args' => array(
+							'orderby' => 'meta_value_num',
+							'meta_key' => 'price',						
+							'order' => 'ASC',
+					)
+			);
+			
+			if(is_array($options) && !empty($options)){
+				foreach($options as $option_key => $options_array){
+					$new_options[$option_key] = $options_array;
+				}
+			}
+		
+			return $new_options;
+		 
+		}else{
+			return $options;	
+		}
+
+
+	}		
+
+	/**
+	 * Filters the travel style main query
+	 *
+	 */
+	public function price_sorting($query) {
+
+		$search_slug = false;
+		if($query->is_search()){
+			$search_slug = 'general';
+			$option_slug = '';
+		}elseif($query->is_post_type_archive($this->post_types)||$query->is_tax($this->taxonomies)){
+			$search_slug = get_query_var('post_type');
+			$option_slug = 'archive_';
+		}
+
+		if (!is_admin() && $query->is_main_query() && false !== $search_slug && false !== $this->options) {
+
+
+			/*if (isset($this->options[$search_slug]['enable_search']) && 'on' === $this->options[$search_slug]['enable_search']) {
+				
+				$query->set('posts_per_page', -1);
+				$query->set('nopaging', true);
+			}*/	
+
+			if (isset($this->options[$search_slug]['enable_'.$option_slug.'price_sorting'])	&& 'on' === $this->options[$search_slug]['enable_'.$option_slug.'price_sorting']){
+
+				$query->set('orderby', 'meta_value_num');
+				$query->set('order', 'DESC');
+				$query->set('meta_key', 'price');	
+				
+				if(isset($_GET['sort'])){
+					$query->set('order', ucwords($_GET['sort']));
+				}
+			}			
+		}
+
+		return $query;
+	}
+	
+
+	/**
+	 * Outputs Search Sidebar.
+	 *
+	 */
+	public function lsx_content_top() { 
+		if(is_search()){
+			$option_slug = '';
+		}elseif(is_post_type_archive($this->post_types) || is_tax($this->taxonomies)){
+			$option_slug = 'archive_';
+		}
+		?>
+		<div class="row" id="facetwp-top">
+			<div class="col-sm-12">
+				<?php echo do_shortcode('[facetwp pager="true"]'); ?>
+			</div>		
+			<div class="col-sm-12">
+				<?php echo do_shortcode('[facetwp sort="true"]'); ?>
+				<?php if(isset($this->options[$this->search_slug][$option_slug.'facets']) && is_array($this->options[$this->search_slug][$option_slug.'facets']) && array_key_exists('a_z',$this->options[$this->search_slug][$option_slug.'facets'])) {echo do_shortcode('[facetwp facet="a_z"]');} ?>
+			</div>
+		</div>
+
+		<div class="facetwp-template">
+
+	<?php 
+	}
+
+	/**
+	 * Outputs Search Sidebar.
+	 *
+	 */
+	public function lsx_content_bottom() { ?>
+		</div>
+		<div class="row" id="facetwp-bottom">
+			<div class="col-sm-12">
+				<?php echo do_shortcode('[facetwp pager="true"]'); ?>
+			</div>
+		</div>
+	<?php 
+	}	
+
+	/**
+	 * Outputs Search Sidebar.
+	 *
+	 */
+	public function search_sidebar() {
+
+		if(false !== $this->search_slug){
+			if(is_search()){
+				$option_slug = '';
+				$facet_slug = '';
+			}elseif(is_post_type_archive($this->post_types) || is_tax($this->taxonomies)){
+				$option_slug = 'archive_';
+			}		
+			?>
+				<div id="secondary" class="facetwp-sidebar widget-area <?php echo esc_attr(lsx_sidebar_class()); ?>" role="complementary">
+
+					<div class="row">
+
+					<?php if(isset($this->options[$this->search_slug]['display_'.$option_slug.'result_count']) && 'on' === $this->options[$this->search_slug]['display_'.$option_slug.'result_count']) { ?>
+						<div class="col-sm-12 col-xs-12 facetwp-results">
+							<h3 class="title"><?php _e('Results',$this->plugin_slug); ?> (<?php echo do_shortcode('[facetwp counts="true"]'); ?>) <button class="btn facetwp-results-clear-btn hidden" type="button" onclick="FWP.reset()">Clear</button></h4>
+						</div>
+					<?php } ?>
+						
+					<?php if(isset($this->options[$this->search_slug][$option_slug.'facets']) && is_array($this->options[$this->search_slug][$option_slug.'facets'])) { 
+
+						foreach($this->options[$this->search_slug][$option_slug.'facets'] as $facet => $facet_useless) {
+							if('a_z' === $facet) {continue; }
+							if('search_form' === $facet){ ?>
+								<div class="col-sm-12 col-xs-12 facetwp-form">
+									<form class="banner-form" action="/" method="get">
+										<div class="input-group">
+											<input class="search-field form-control" name="s" type="search" placeholder="<?php _e('Search',$this->plugin_slug); ?>..." autocomplete="off" value="<?php echo get_search_query() ?>">
+											<?php if('general' !== $this->search_slug) { ?>
+												<input name="engine" type="hidden" value="<?php echo $this->search_slug; ?>">
+											<?php } ?>
+											<span class="input-group-btn"><button class="search-submit btn cta-btn" type="submit"><?php _e('Search',$this->plugin_slug); ?></button></span>
+										</div>
+									</form>	
+								</div>
+							<?php }else{ ?>
+								<div class="col-sm-12 col-xs-6">
+									<h3 class="title"><?php echo $this->facet_data[$facet]['label']; ?></h3>
+									<?php echo do_shortcode('[facetwp facet="'.$facet.'"]'); ?>
+								</div>
+							<?php }
+						}
+					} ?>	
+
+					<?php if(isset($this->options[$this->search_slug]['display_'.$option_slug.'result_count']) && 'on' === $this->options[$this->search_slug]['display_'.$option_slug.'result_count'] && $this->options[$this->search_slug]['search_layout'] != '1c') { ?>
+						<div class="col-sm-12 col-xs-12 facetwp-results">
+							<h3 class="title"><?php _e('Results',$this->plugin_slug); ?> (<?php echo do_shortcode('[facetwp counts="true"]'); ?>) <button class="btn facetwp-results-clear-btn hidden" type="button" onclick="FWP.reset()">Clear</button></h4>
+						</div>
+					<?php } ?>
+					</div>
+				</div>
+
+			<?php
+		}
+	}
+
+	/**
+	 * Outputs the appropriate search form
+	 */
+	public function search_form( $atts = array() ){
+		
+		$classes = 'search-form ';
+		if(isset($atts['class'])){ $classes .= $atts['class']; }
+
+		$placeholder = __('Where do you want to go?',$this->plugin_slug);
+		if(isset($atts['placeholder'])){ $placeholder = $atts['placeholder']; }	
+
+		$action = '/';
+		if(isset($atts['action'])){ $action = $atts['action']; }	
+
+		$method = 'get';
+		if(isset($atts['method'])){ $method = $atts['method']; }	
+
+		$button_label = __('Search',$this->plugin_slug);
+		if(isset($atts['button_label'])){ $button_label = $atts['button_label']; }
+
+		$button_class = 'btn cta-btn ';
+		if(isset($atts['button_class'])){ $button_class .= $atts['button_class']; }	
+
+		$engine = false;
+		if(isset($atts['engine'])){ $engine = $atts['engine']; }				
+
+		$return = '';
+
+		ob_start(); ?>
+
+			<?php do_action('lsx_search_form_before'); ?>
+
+			<form class="<?php echo $classes; ?>" action="<?php echo $action; ?>" method="<?php echo $method; ?>">
+
+			<?php do_action('lsx_search_form_top'); ?>
+
+				<div class="input-group">
+					<input class="search-field form-control" name="s" type="search" placeholder="<?php echo $placeholder; ?>" autocomplete="off">
+					<?php if(false !== $engine && 'default' !== $engine){ ?>
+						<input name="engine" type="hidden" value="<?php echo $engine; ?>">
+					<?php } ?>
+					<span class="input-group-btn"><button class="<?php echo $button_class; ?>" type="submit"><?php echo $button_label; ?></button></span>
+				</div>
+
+			<?php do_action('lsx_search_form_bottom'); ?>	
+				
+			</form>	
+
+			<?php do_action('lsx_search_form_after'); ?>
+		<?php
+		$return = ob_get_clean();
+
+		return $return;
+	}		
+
+	/**
+	 * Outputs closing facetWP div.
+	 *
+	 */
+	public function lsx_sidebar_enable($return) {
+		if(false !== $this->search_slug){
+			$return = 0;
+		}
+		return $return;
+	}
+
+	/**
+	 * Change FaceWP pagination HTML to be equal main pagination (WP-PageNavi)
+	 */
+	public function facetwp_pager_html( $output, $params ) {
+	    $output = '';
+	    $page = (int) $params['page'];
+	    $per_page = (int) $params['per_page'];
+	    $total_pages = (int) $params['total_pages'];
+
+	    if ( 1 < $total_pages ) {
+	        $output .= '<div class="wp-pagenavi-wrapper facetwp-custom">';
+	        $output .= '<div class="lsx-breaker"></div>';
+	        $output .= '<div class="wp-pagenavi">';
+	        $output .= '<span class="pages">Page '. $page .' of '. $total_pages .'</span>';
+
+	        if ( 1 < $page ) {
+	            $output .= '<a class="previouspostslink facetwp-page" rel="prev" data-page="'. ( $page - 1 ) .'">«</a>';
+	        }
+
+	        $temp = false;
+	        
+	        for ( $i = 1; $i <= $total_pages; $i++ ) {
+	            if ( $i == $page ) {
+	                $output .= '<span class="current">'. $i .'</span>';
+	            } elseif ( ( $page - 5 ) < $i && ( $page + 5 ) > $i ) {
+	                $output .= '<a class="page larger facetwp-page" data-page="'. $i .'">'. $i .'</a>';
+	            } elseif ( ( $page - 5 ) >= $i && $page > 5 ) {
+	                if ( ! $temp ) {
+	                    $output .= '<span>...</span>';
+	                    $temp = true;
+	                }
+	            } elseif ( ( $page + 5 ) <= $i && ( $page + 5 ) <= $total_pages ) {
+	                $output .= '<span>...</span>';
+	                break;
+	            }
+	        }
+
+	        if ( $page < $total_pages ) {
+	            $output .= '<a class="nextpostslink facetwp-page" rel="next" data-page="'. ( $page + 1 ) .'">»</a>';
+	        }
+
+	        $output .= '</div>';
+	        $output .= '</div>';
+	    }
+
+	    return $output;
+	}
+	
+
+	/**
+	 * Change FaceWP result count HTML
+	 */
+	function facetwp_result_count( $output, $params ) {
+		$output = $params['total'];
+		return $output;
+	}	
+
+	/**
+	 * Displays the destination specific settings
+	 */
+	public function facetwp_index_row( $params, $class ) {
+
+		$custom_field = $meta_key = false;
+		preg_match("/cf\//", $class->facet['source'], $custom_field);
+		preg_match("/_to_/", $class->facet['source'], $meta_key);
+
+		if(!empty($custom_field) && !empty($meta_key)){
+	        $params['facet_display_value'] = get_the_title($params['facet_value']);
+	    }
+	    return $params;
+	}	
+
+	/**
+	 * Change FaceWP result count HTML
+	 */
+	function get_search_query( $keyword ) {
+		$engine = get_query_var('engine_keyword');
+		if(false !== $engine && '' !== $engine && 'default' !== $engine){
+			$keyword = $engine;
+		}
+		return $keyword;
+	}			
 }
 new LSX_Search_Frontend();
