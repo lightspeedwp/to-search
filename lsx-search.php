88aa068ff8d1ec038d595df0d56934e914c71424
Warwick Booth <warwick@lsdev.biz>
2016-09-20 11:51:52 +0200

    Search API Enabling

--- lsx-search.php
+++ lsx-search.php
@@ -34,6 +34,40 @@ if(!class_exists('LSX_API_Manager')){
 	require_once('classes/class-lsx-api-manager.php');
 }
 
+/** 
+ *	Grabs the email and api key from the LSX Search Settings.
+ */ 
+function lsx_search_options_pages_filter($pages){
+	$pages[] = 'lsx-lsx-settings';
+	return $pages;
+}
+add_filter('lsx_api_manager_options_pages','lsx_search_options_pages_filter',10,1);
+
+function lsx_search_api_admin_init(){
+	$options = get_option('_lsx_lsx-settings',false);
+	$data = array('api_key'=>'','email'=>'');
+
+	if(false !== $options && isset($options['general'])){
+		if(isset($options['general']['lsx-search_api_key']) && '' !== $options['general']['lsx-search_api_key']){
+			$data['api_key'] = $options['general']['lsx-search_api_key'];
+		}
+		if(isset($options['general']['lsx-search_email']) && '' !== $options['general']['lsx-search_email']){
+			$data['email'] = $options['general']['lsx-search_email'];
+		}		
+	}
+
+	$api_array = array(
+		'product_id'	=>		'LSX Search',
+		'version'		=>		'1.0.0',
+		'instance'		=>		get_option('lsx_api_instance',false),
+		'email'			=>		$data['email'],
+		'api_key'		=>		$data['api_key'],
+		'file'			=>		'lsx-search.php'
+	);
+	$lsx_to_api_manager = new LSX_API_Manager($api_array);
+}
+add_action('admin_init','lsx_search_api_admin_init');
+
 /* ======================= Below is the Plugin Class init ========================= */
 
 require_once( LSX_SEARCH_PATH . '/classes/class-lsx-search.php' );
